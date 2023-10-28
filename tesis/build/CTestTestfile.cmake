99bc6eb6ec9bf4b0097494d7a4fd1f88f8957091
Maria Vera Tudela <88990737+mariaveratudela@users.noreply.github.com>
2023-10-28 08:30:41 -0500

    Add files via upload

---
+++ tesis/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/maria/tesis/src
+# Build directory: /home/maria/tesis/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("omni_description")
+subdirs("rplidar_ros")
